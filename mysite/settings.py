01fda2b527ee02adf2f1ab4a296a28ee873886a8
Bart Cubrich <41648759+Bcubrich@users.noreply.github.com>
2020-09-07 11:10:07 -0600

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -32,7 +32,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 DEFAULT_FROM_EMAIL = "bcubrich@gmail.com"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_HOST_USER = "bcubrich@gmail.com"
-EMAIL_HOST_PASSWORD = 'Noh3ro3s'
+EMAIL_HOST_PASSWORD = '***********'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
